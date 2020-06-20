e0f524f24bffdfe521049affbeeb59eeb26f7330
reeya3 <manandharreeya@gmail.com>
2020-06-20 15:52:04 +0545

    user model and validation

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_617_024_747) do
+ActiveRecord::Schema.define(version: 20_200_620_073_841) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+  end
+
+  create_table 'users', force: :cascade do |t|
+    t.string 'username'
+    t.string 'email'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
   end
